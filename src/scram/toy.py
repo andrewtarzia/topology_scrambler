8ca7092ba7a4f9d6bd00a782ad09125967e1ed22
andrewtarzia <andrew.tarzia@gmail.com>
2024-09-18 16:33:02 +0200

    Rename file.

--- src/scram/toy.py
+++ src/scram/toy.py
@@ -1,6 +1,6 @@
 """analysis package."""
 
-from scram._internal.toy.min_utilities import (
+from scram._internal.toy.utilities import (
     graph_optimise_cage,
     optimise_cage,
     prepare_building_block,
