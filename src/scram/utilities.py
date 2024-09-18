daa06d1d125c0be7cb1d43cf64a26535163771b6
andrewtarzia <andrew.tarzia@gmail.com>
2024-09-18 12:03:43 +0200

    Some refactoring.

--- src/scram/utilities.py
+++ src/scram/utilities.py
@@ -2,18 +2,7 @@
 
 from scram._internal.utilities.utilities import (
     atomise,
-    extract_ensemble,
-    get_ligand_bb,
     get_vertex_positions,
-    optimisation_sequence,
-    plot_xy,
 )
 
-__all__ = [
-    "extract_ensemble",
-    "optimisation_sequence",
-    "plot_xy",
-    "get_vertex_positions",
-    "atomise",
-    "get_ligand_bb",
-]
+__all__ = ["get_vertex_positions", "atomise"]
