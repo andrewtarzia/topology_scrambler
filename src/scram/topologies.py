a38001b2b3fb07c7417065f6c2b29fc4a7f72ad7
andrewtarzia <andrew.tarzia@gmail.com>
2024-09-18 12:03:58 +0200

    Some refactoring.

--- src/scram/topologies.py
+++ src/scram/topologies.py
@@ -1,5 +1,8 @@
 """analysis package."""
 
+from scram._internal.topologies.building_block_enum import (
+    get_potential_bb_dicts,
+)
 from scram._internal.topologies.enumeration import (
     Constructed,
     CustomTopology,
@@ -12,7 +15,11 @@ from scram._internal.topologies.enumeration import (
 from scram._internal.topologies.graphs import (
     CGM4L8,
     CGM12L24,
+    M4L82,
+    M6L122,
+    M8L162,
     UnalignedM1L2,
+    stoich_map,
 )
 from scram._internal.topologies.utilities import (
     get_underyling_vertices,
@@ -21,12 +28,17 @@ from scram._internal.topologies.utilities import (
 
 __all__ = [
     "get_underyling_vertices",
+    "get_potential_bb_dicts",
     "get_graph_type",
     "vmap_to_str",
+    "stoich_map",
     "CustomTopology",
     "UnalignedM1L2",
     "CGM4L8",
     "CGM12L24",
+    "M8L162",
+    "M6L122",
+    "M4L82",
     "TopologyCode",
     "Constructed",
     "TopologyIterator",
