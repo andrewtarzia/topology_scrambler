5d1149da6f4b4f94b670e11f407eea39bc45c763
andrewtarzia <andrew.tarzia@gmail.com>
2024-10-02 21:41:35 +0200

    Remove module.

--- src/scram/_internal/utilities/rustwork.py
+++
@@ -1,9 +0,0 @@
-"""Module for working with rustworkx."""
-
-from dataclasses import dataclass
-
-
-@dataclass
-class Rustwork:
-    def get_connected_components(self) -> int:
-        """Get the number of connected components."""
