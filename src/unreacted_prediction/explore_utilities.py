cb2fe83f03809566cb508619c9e805d5ea680cb3
andrewtarzia <andrew.tarzia@gmail.com>
2024-12-29 23:04:33 +0100

    Handle missing torsion controls.

--- src/unreacted_prediction/explore_utilities.py
+++ src/unreacted_prediction/explore_utilities.py
@@ -121,12 +121,12 @@ def precursors_to_forcefield(
         definer_dict["eg"] = ("bond", ditopic_meas["eg"] / cg_scale, 1e5)
         definer_dict["gb"] = ("bond", ditopic_meas["gb"] / cg_scale, 1e5)
         definer_dict["dde"] = ("angle", ditopic_meas["dde"], 1e2)
-        if "edde_v" in ditopic_meas:
+        if "edde_v" in ditopic_meas or "edde_k" in ditopic_meas:
             definer_dict["edde"] = (
                 "tors",
                 "0123",
-                ditopic_meas["edde_v"],
-                ditopic_meas["edde_k"],
+                ditopic_meas.get("edde_v", 180),
+                ditopic_meas.get("edde_k", 50),
                 1,
             )
         else:
