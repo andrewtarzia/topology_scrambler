f3ac29547b2f2b66f4e2f7a8fd75a6145407e034
andrewtarzia <andrew.tarzia@gmail.com>
2025-02-05 22:48:09 +0100

    Move beyond ufo.

--- src/model_enumeration/mgen_crest_analysis.py
+++ src/model_enumeration/mgen_crest_analysis.py
@@ -133,11 +133,11 @@ def plot_distance_angle(
 def main() -> None:
     """Run script."""
     wd = pathlib.Path("/home/atarzia/workingspace/model_enum_data/")
-    ligand_dir = wd / "ufo_aa_ligands"
+    ligand_dir = wd / "mgen_aa_ligands"
     ligand_dir.mkdir(exist_ok=True)
-    figure_dir = wd / "figures" / "ufo_aa"
+    figure_dir = wd / "figures" / "mgen_aa"
     figure_dir.mkdir(exist_ok=True)
-    calculation_dir = wd / "ufo_aa_calculations"
+    calculation_dir = wd / "mgen_aa_calculations"
     calculation_dir.mkdir(exist_ok=True)
     crest_path = pathlib.Path("/home/atarzia/software/crest_301/crest")
     xtb_path = pathlib.Path(
@@ -145,31 +145,68 @@ def main() -> None:
     )
 
     ligands = {
-        "lf": {
-            "smiles": (
-                "C1=C(C2=CC=C(C3C=CC4C(=O)C5C=CC(C6=CC=C(C7=CC=CN=C7)C=C6)=CC"
-                "=5C=4C=3)C=C2)C=NC=C1"
-            ),
-        },
-        "ls1": {"smiles": "C1=CC(=CC(=C1)C2=CC=NC=C2)C3=CC=NC=C3"},
-        "ls2": {"smiles": "C1=CC(=NC(=C1)C2=CC=NC=C2)C3=CC=NC=C3"},
-        "ls3": {"smiles": "C1=CC(=C(C(=C1)C2=CC=NC=C2)N)C3=CC=NC=C3"},
-        "ls4": {"smiles": "N1=CC=C(C2=C(OC)C(C3=CC=NC=C3)=CC=C2)C=C1"},
-        "ls5": {"smiles": "C1=CC(=C(C(=C1)C2=CC=NC=C2)O)C3=CC=NC=C3"},
-        "ls7": {
-            "smiles": "C1(C(C2C=CN=CC=2)=CC=CC=1C1C=CN=CC=1)OC(=O)C(C)(C)C"
-        },
-        "ls8": {
-            "smiles": "C1(C(C2C=CN=CC=2)=CC=CC=1C1C=CN=CC=1)OC(=O)C1C=CC=CC=1"
-        },
-        "ls9": {"smiles": "C1=CN=CC=C1C2=CC=C(S2)C3=CC=NC=C3"},
-        "ls10": {"smiles": "C1=CN=CC=C1C2=CC=C([Se]2)C3=CC=NC=C3"},
+        "lf": (
+            "C1=C(C2=CC=C(C3C=CC4C(=O)C5C=CC(C6=CC=C(C7=CC=CN=C7)C=C6)=CC"
+            "=5C=4C=3)C=C2)C=NC=C1"
+        ),
+        "ls2": "C1=CC(=NC(=C1)C2=CC=NC=C2)C3=CC=NC=C3",
+        "ls3": "C1=CC(=C(C(=C1)C2=CC=NC=C2)N)C3=CC=NC=C3",
+        "ls4": "N1=CC=C(C2=C(OC)C(C3=CC=NC=C3)=CC=C2)C=C1",
+        "ls5": "C1=CC(=C(C(=C1)C2=CC=NC=C2)O)C3=CC=NC=C3",
+        "ls7": "C1(C(C2C=CN=CC=2)=CC=CC=1C1C=CN=CC=1)OC(=O)C(C)(C)C",
+        "ls8": "C1(C(C2C=CN=CC=2)=CC=CC=1C1C=CN=CC=1)OC(=O)C1C=CC=CC=1",
+        "ls10": "C1=CN=CC=C1C2=CC=C([Se]2)C3=CC=NC=C3",
+        # Diverging-tarzia_2024.
+        "l1": "C1=NC=CC(C2=CC=C3OC4C=CC(C5C=CN=CC=5)=CC=4C3=C2)=C1",
+        "l2": "C1=CC(=CC(=C1)C2=CC=NC=C2)C3=CC=NC=C3",
+        "l3": "C1=CN=CC=C1C2=CC=C(S2)C3=CC=NC=C3",
+        # Converging-tarzia_2024.
+        "la": (
+            "C1=CN=CC2C(C3=CC=C(C#CC4=CC5C6C=C(C#CC7=CC=C(C8=CC=CC9C=C"
+            "N=CC8=9)C=C7)C=CC=6OC=5C=C4)C=C3)=CC=CC1=2"
+        ),
+        "lb": (
+            "C1=CN=CC2C(C3=CC=C(C#CC4N=C(C#CC5=CC=C(C6=CC=CC7C=CN=CC6="
+            "7)C=C5)C=CC=4)C=C3)=CC=CC1=2"
+        ),
+        "lc": (
+            "C1C2=C(C(=CC=C2)C2C=CC(C#CC3=CC=CC(C#CC4C=CC(C5C6=C(C=CN="
+            "C6)C=CC=5)=CC=4)=C3)=CC=2)C=NC=1"
+        ),
+        "ld": (
+            "C1C2=C(C(=CC=C2)C2C=CC(C#CC3=CC=C(C#CC4C=CC(C5C6=C(C=CN=C"
+            "6)C=CC=5)=CC=4)S3)=CC=2)C=NC=1"
+        ),
+        # Experimental.
+        "e10": (
+            "C1=CC(C#CC2=CC3C4C=C(C#CC5=CC=CN=C5)C=CC=4N(C)C=3C=C2)=CN=C1"
+        ),
+        "e11": ("C1N=CC=CC=1C1=CC2=C(C3=C(C2(C)C)C=C(C2=CN=CC=C2)C=C3)C=C1"),
+        "e12": "C1=CC=C(C2=CC3C(=O)C4C=C(C5=CN=CC=C5)C=CC=4C=3C=C2)C=N1",
+        "e13": (
+            "C1C=C(N2C(=O)C3=C(C=C4C(=C3)C3(C5=C(C4(C)CC3)C=C3C(C(N(C3="
+            "O)C3C=CC=NC=3)=O)=C5)C)C2=O)C=NC=1"
+        ),
+        "e14": (
+            "C1=CN=CC(C#CC2C=CC3C(=O)C4C=CC(C#CC5=CC=CN=C5)=CC=4C=3C=2)=C1"
+        ),
+        "e16": (
+            "C(C1=CC2C3C=C(C4=CC=NC=C4)C=CC=3C(OC)=C(OC)C=2C=C1)1=CC=NC=C1"
+        ),
+        "e17": (
+            "C12C=CN=CC=1C(C#CC1=CC=C3C(C(C4=C(N3C)C=CC(C#CC3=CC=CC5C3="
+            "CN=CC=5)=C4)=O)=C1)=CC=C2"
+        ),
+        "e18": (
+            "C1(=CC=NC=C1)C#CC1=CC2C3C=C(C#CC4=CC=NC=C4)C=CC=3C(OC)=C(O"
+            "C)C=2C=C1"
+        ),
     }
 
     ensembles = {}
-    for ligand, ldict in ligands.items():
+    for ligand, lsmiles in ligands.items():
         logging.info("doing %s", ligand)
-        molecule = stk.BuildingBlock(ldict["smiles"])
+        molecule = stk.BuildingBlock(lsmiles)
 
         molecule.write(ligand_dir / f"{ligand}_unopt.mol")
 
@@ -185,7 +222,7 @@ def main() -> None:
             xtb_path=xtb_path,
         )
         ensembles[ligand] = ensemble
-
+    raise SystemExit
     plot_distance_angle(
         ensembles=ensembles,
         figure_dir=figure_dir,
